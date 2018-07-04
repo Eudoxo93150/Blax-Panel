974df0a49ab1fab330a1bf2ee15dcab3099c66eb
TheCrazy99 <garzon93150@gmail.com>
2018-07-03 22:12:48 -0500

    Sistema VIP Con Id, Pass, Hwid PHP/VB.NET

---
+++ BlaxPanel Login VB.NET/BlaxPanel Login/Form1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Management
+
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim client As New System.Net.WebClient
+        Dim date_ As String = DateTime.Now.ToString("yyyy/MM/dd")
+        Dim _data As String = client.DownloadString("http://localhost/BlaxPanel/handler.php?action=login&username=" + TextBox1.Text + "&password=" + TextBox2.Text + "&hwid=" + GetHWID())
+        If _data = "true" Then
+            MsgBox("Bienvenido")
+        Else
+            MsgBox("Datos Incorrectos")
+        End If
+    End Sub
+    Dim cpuInfo As String
+    Function GetHWID()
+        Dim mc As New ManagementClass("win32_processor")
+        Dim moc As ManagementObjectCollection = mc.GetInstances
+        For Each mo As ManagementObject In moc
+            If cpuInfo = "" Then
+                cpuInfo = mo.Properties("processorID").Value.ToString
+                Exit For
+            End If
+        Next
+        Return cpuInfo
+    End Function
+End Class
---
+++ GETHWID VB.NET/GETHWID/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Management
+Imports InjectionLibrary
+Imports JLibrary.PortableExecutable
+
+Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        TextBox1.Text = GetHWID()
+    End Sub
+    Dim cpuInfo As String
+    Function GetHWID()
+        Dim mc As New ManagementClass("win32_processor")
+        Dim moc As ManagementObjectCollection = mc.GetInstances
+        For Each mo As ManagementObject In moc
+            If cpuInfo = "" Then
+                cpuInfo = mo.Properties("processorID").Value.ToString
+                Exit For
+            End If
+        Next
+        Return cpuInfo
+    End Function
+End Class
